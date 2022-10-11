ff639ea972fe6574d54bf6418b1615df2b0b2ec1
dimov05 <ddimov.1999@gmail.com>
2022-10-11 08:40:39 +0300

    Data definition and data types exercise

---
+++ Database_Basics_Softuni/02_Data_Definition_And_Data_Types_Exercises/01_Create_Tables_In_Database_Minions.sql
@@ -0,0 +1,11 @@
+USE `minions`;
+
+CREATE TABLE `minions` (
+	`id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `age` INT NOT NULL
+);
+CREATE TABLE `towns` (
+	`town_id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50)
+);
