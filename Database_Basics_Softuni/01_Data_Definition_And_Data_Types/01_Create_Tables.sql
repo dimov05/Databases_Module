002f2d5c7ecfa6808047841155d4226346c445fa
dimov05 <ddimov.1999@gmail.com>
2022-10-11 08:35:01 +0300

    Data definition and data types

---
+++ Database_Basics_Softuni/01_Data_Definition_And_Data_Types/01_Create_Tables.sql
@@ -0,0 +1,3 @@
+INSERT INTO `gamebar`.`employees` (`id`, `first_name`, `last_name`) VALUES ('1', 'Ivan', 'Petrov');
+INSERT INTO `gamebar`.`employees` (`id`, `first_name`, `last_name`) VALUES ('2', 'Georgi', 'Dimitrov');
+INSERT INTO `gamebar`.`employees` (`id`, `first_name`, `last_name`) VALUES ('3', 'Dimitar', 'Georgiev');
