4b77019e460678b8b41a4801b0581086cf0d81ca
dimov05 <ddimov.1999@gmail.com>
2022-10-26 08:42:00 +0300

    Table Relations

---
+++ Database_Basics_Softuni/09_Table_Relations/01_Mountains_And_Peaks_Create_Tables_And_Foreign_Key.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `mountains` (
+`id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+`name` VARCHAR(30) NOT NULL
+);
+CREATE TABLE `peaks` (
+`id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+`name` VARCHAR(30) NOT NULL,
+`mountain_id` INT NOT NULL,
+CONSTRAINT fk_mountain_id
+FOREIGN KEY (`mountain_id`)
+REFERENCES `mountains`(`id`)
+);
