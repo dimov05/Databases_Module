3a8846def0da2940c9d394dddd2a057eb507f4a6
dimov05 <ddimov.1999@gmail.com>
2022-10-28 09:05:28 +0300

    Subqueries and Joins exercises

---
+++ Database_Basics_Softuni/12_Subqueries_And_Joins_Exercise/02_Addresses_With_Towns_Double_Join.sql
@@ -0,0 +1,13 @@
+SELECT 
+    e.`first_name`,
+    e.`last_name`,
+    t.`name` AS `town`,
+    a.`address_text`
+FROM
+    `employees` AS e
+        JOIN
+    `addresses` AS a ON a.`address_id` = e.`address_id`
+        JOIN
+    `towns` AS t ON t.`town_id` = a.`town_id`
+ORDER BY e.`first_name` , e.`last_name`
+LIMIT 5;
