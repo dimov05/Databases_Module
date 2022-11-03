b25846006fa76cd0e5eb446bd2b08344efef60c4
dimov05 <ddimov.1999@gmail.com>
2022-11-03 14:56:09 +0200

    Exam April 2022

---
+++ Database_Basics_Softuni/15_Exam_April_2022/2_Insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO `actors`(first_name,last_name,birthdate,height,awards,country_id)
+SELECT (REVERSE(a.first_name)),(REVERSE(a.last_name)),(DATE(a.birthdate-2)),(a.height+10),(a.country_id),(3) FROM `actors` as a
+WHERE a.id <= 10;
