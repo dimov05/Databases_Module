fb1c1204d81da5483aa537f22bd5d8a81db6b797
dimov05 <ddimov.1999@gmail.com>
2022-12-01 08:08:15 +0200

    Spring intro exercise

---
+++ Spring_Data/spring-intro-ex/src/main/java/com/softuni/springintroex/utils/FileUtilImpl.java
@@ -0,0 +1,28 @@
+package com.softuni.springintroex.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Component
+public class FileUtilImpl implements FileUtil {
+
+
+    @Override
+    public String[] readFileContent(String filePath) throws IOException {
+        File file = new File(filePath);
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        Set<String> result = new LinkedHashSet<>();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (!"".equals(line)) {
+                result.add(line);
+            }
+        }
+
+        return result.toArray(String[]::new);
+    }
+}
