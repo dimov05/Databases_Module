1c3f4ef93e688dae428da5526dfa40112cb2abf5
dimov05 <ddimov.1999@gmail.com>
2022-12-06 07:59:58 +0200

    Xml demo lab

---
+++ Spring_Data/xml-demo-lab/src/main/java/course/springdata/xmldemo/model/Person.java
@@ -0,0 +1,27 @@
+package course.springdata.xmldemo.model;
+
+import lombok.*;
+
+import javax.xml.bind.annotation.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Person {
+    @XmlAttribute(required = true)
+    private Long id;
+    @NonNull
+    private String firstName;
+    @NonNull
+    private String lastName;
+    @NonNull
+    private Address address;
+    @XmlElementWrapper(name = "phone-numbers")
+    @XmlElement(name = "phone")
+    private Set<PhoneNumber> phoneNumbers = new HashSet<>();
+}
